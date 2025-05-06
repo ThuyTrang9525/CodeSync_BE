fa6cad2377840e19107ffeb7d61382477b56ecb1
MinhHoang1225 <minhhoang121225@gmail.com>
2025-05-06 08:49:55 +0700

    Hoang | done api student

--- routes/api.php
+++ routes/api.php
@@ -4,25 +4,17 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\StudentController;
+use App\Http\Controllers\AdminDashboardController;
 
-// API Routes cho Student
+
+
 Route::apiResource('student', StudentController::class);  // Sử dụng apiResource để tự động tạo các route
 
-/*
-Test API 
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will be
-| assigned to the "api" middleware group. Make something great!
-|
-*/
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// API Routes cho User
-Route::apiResource('user', UserController::class);  // Thay thế các route cũ cho user bằng apiResource
+Route::apiResource('user', UserController::class);
+
+Route::get('/dashboard', [AdminDashboardController::class, 'getStats']);
